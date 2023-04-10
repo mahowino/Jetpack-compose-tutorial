4513fe6ba63e92e64ff039368adae755528072af
poolr-ridesharing <84660656+poolr-ridesharing@users.noreply.github.com>
2023-04-10 14:50:54 +0300

    rows, columns and modifiers

--- app/src/main/java/online/kenya/m_vituApp/MainActivity.kt
+++ app/src/main/java/online/kenya/m_vituApp/MainActivity.kt
@@ -3,20 +3,25 @@ package online.kenya.m_vituApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import online.kenya.m_vituApp.ui.theme.MvituTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "mahalon")
+
+
         }
     }
 }
@@ -29,5 +34,14 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-   Greeting(name = "mahalon")
+    Column(
+        modifier=Modifier
+            .width(300.dp)
+            .fillMaxHeight()
+            .background(Color.Green),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Greeting(name = "mahalon")
+        Greeting(name = "age")
+    }
 }
